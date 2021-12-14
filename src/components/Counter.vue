5b03a073593049e31cae857b6ca836a9679527dc
iCatOK <kamilyen5@gmail.com>
2021-12-14 08:50:50 +0500

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  .wrapper
+    h1 Счётчик
+    h2 {{counter}}
+    button(@click="stopTimer") {{buttonText}}
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data () {
+    return {
+      counter: 0,
+      setIntervalInstance: null,
+      isCounting: true,
+      buttonText: "Остановить"
+    }
+  },
+  mounted () {
+    this.setIntervalInstance = setInterval(() => {
+      this.counter++
+    }, 1000)
+  },
+  methods: {
+    stopTimer(){
+      if(this.isCounting){
+        clearInterval(this.setIntervalInstance);
+        this.buttonText = "Запустить"
+      } else {
+        this.setIntervalInstance = setInterval(() => {
+          this.counter++
+        }, 1000)
+        this.buttonText = "Остановить"
+      }
+      this.isCounting = !this.isCounting;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
